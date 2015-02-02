a9943e6da0d290a3261d6f29c1a00ad195a2ed4b
Etan Washbourne <ewashbourne@gmail.com>
2015-02-02 17:56:19 -0500

    ported over paddler sinatra app

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,13 @@
+class ItemsController < ApplicationController
+
+	def index
+		@active = 'items'
+		@items = Inventory.all 
+	end
+
+	def show
+		@active = 'items'
+		@item = Inventory.find(params[:id].to_i)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'static_pages#home'
+
+  get '/items',     to: 'items#index'
+
+  get '/items/:id', to: 'items#show'
+
+  get '/about', to: 'static_pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
